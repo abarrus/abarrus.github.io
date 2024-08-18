05b6f1c27d7d206f027e8b45d4c283d2007136c3
abarrus <67875711+abarrus@users.noreply.github.com>
2024-08-18 09:29:43 -0400

    Update main.js
    Comment out some lines

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router';  // Optional, if you're using Vue Router
-import store from './store';    // Optional, if you're using Vuex
+//import router from './router';  // Optional, if you're using Vue Router
+//import store from './store';    // Optional, if you're using Vuex
 
 Vue.config.productionTip = false;
 

db07323e1d468157fb3e8607ce54bc1599dba605
abarrus <67875711+abarrus@users.noreply.github.com>
2024-08-18 09:24:38 -0400

    Create main.js
    Created using chatGPT for testing purposes

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';  // Optional, if you're using Vue Router
+import store from './store';    // Optional, if you're using Vuex
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,  // Optional
+  store,   // Optional
+  render: h => h(App),
+}).$mount('#app');
